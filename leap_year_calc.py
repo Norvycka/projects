35920d85c2b7b436a4be4fc36424921e5976f97a
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-09-24 14:10:13 +0300

    Update leap_year_calc.py
    Fixed mistake, elif was a bad choice, I had to have additional else statement for 100 module to check if was a leap year.

--- leap_year_calc.py
+++ leap_year_calc.py
@@ -1,12 +1,13 @@
 year = int(input("Which year do you want to check? "))
 
 if year % 4 == 0:
-    print("Leap year.")
-elif year % 100 == 0:
-    if year % 400 == 0:
-        print("Leap year.")
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
     else:
-        print("Not leap year.")
+        print("Leap year.")
 else:
     print("Not leap year.")
 
