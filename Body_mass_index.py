d748fb01298565fce5d8533867d82c7022b3cd1a
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-09-26 08:58:11 +0300

    Update Body_mass_index.py
    fixing gaps

--- Body_mass_index.py
+++ Body_mass_index.py
@@ -12,5 +12,6 @@ elif 25 < bmi <= 30:
     print(f"Your BMI is {bmi}, you are slightly overweight")
 elif 30 < bmi <= 35:
     print(f"Your BMI is {bmi}, you are obese.")
+    
 else:
     print(f"Your BMI is {bmi}, you are clinically obese.")
