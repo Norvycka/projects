d1ef2e285b140c2de017c12e57831cabf89c498b
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-10-01 14:44:50 +0300

    find a highscore
    for in loop and lists practice.

---
+++ highscore.py
@@ -0,0 +1,21 @@
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+#get the number of student scores there is:
+
+score_num = 0
+for score in student_scores:
+    score_num += 1
+#print(f"number of student scores= {score_num}")
+
+#find the highest score:
+
+high = 0
+
+for n in range(0, score_num):
+    if high < student_scores[n]:
+        high = student_scores[n]
+        
+print(f"The highest score in the class is: {high}")
