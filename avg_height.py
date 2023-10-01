16d0772b6e036e36215ee83fe47f073a3cf8121f
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-10-01 09:15:10 +0300

    Update avg_height.py
    adding comments to my loop and list learning

--- avg_height.py
+++ avg_height.py
@@ -6,8 +6,13 @@ total = 0
 for n in range(0, len(student_heights)):
     total = total + student_heights[n]
     
+#instead of len() we could have used:
+#number_of_students = 0
+#for student in student_heights:
+    #number_of_students += 1
+#print(f"number of students= {number_of_students}")
+    
 avg_height = total / (int(len(student_heights)))
     
 print(total)
 print(round(avg_height))
-      
