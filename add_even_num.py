8aae7dad707ce716c920b03cf133dd2d40310df9
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-10-02 11:07:03 +0300

    Update add_even_num.py

--- add_even_num.py
+++ add_even_num.py
@@ -1,5 +1,5 @@
 number = 100
-#if you want to choose number on your own uncomment the line below and comment first line
+#if you want to choose number on your own uncomment the line below and comment the first line
 #number = int(input("Input the number you want to sum all the even numbers: "))
 total = 0
 for even in range(2, number+1, 2):
