9845aa5c4f4bf32ffa1f085465dbfe2b7344fe37
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-10-02 11:06:37 +0300

    Update add_even_num.py
    Adding a choice to input your own number into the for loop

--- add_even_num.py
+++ add_even_num.py
@@ -1,4 +1,6 @@
 number = 100
+#if you want to choose number on your own uncomment the line below and comment first line
+#number = int(input("Input the number you want to sum all the even numbers: "))
 total = 0
 for even in range(2, number+1, 2):
     total += even
