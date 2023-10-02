2ed5fc9b9308a53d25df52d97902a4430a6dad57
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-10-02 11:35:21 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,7 @@
-num = int(input("Choose the number! "))
+#num = int(input("Choose the number! "))
+#uncomment first line and comment third line if you want to use your own input number
+
+num = 100
 
 for numbers in range(1, num+1):
     if numbers % 3 == 0 and numbers % 5 == 0:
