bc4269e4848a3646e1944cdb2affbe4341962a49
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-10-05 01:43:32 +0300

    Update Reeborgs_hurdle_3.py

--- Reeborgs_hurdle_3.py
+++ Reeborgs_hurdle_3.py
@@ -1,8 +1,15 @@
-#at_goal() was predefined
-
 def turn_right():
     turn_left()
     turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
     turn_left()
 
 
@@ -10,10 +17,4 @@ while not at_goal():
     if front_is_clear():
         move()
     else:
-        turn_left()
-        move()
-        turn_right()
-        move()
-        turn_right()
-        move()
-        turn_left()
+        jump()
