ea66569dbd3f7d8f50c29750500e27cf8091429c
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-10-18 08:42:22 +0300

    Update hangman_part_1_redo.py

--- hangman_part_1_redo.py
+++ hangman_part_1_redo.py
@@ -4,15 +4,32 @@ word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
-#random_choice = random.randint(0, len(word_list))
 #chosen_word = word_list[random_choice]
 
-guess = input("Guess the letter: ").lower()
+print(f'Pssst, the solution is {chosen_word}.')
+
 
 #chosen_word_length = len((chosen_word))
 
-for n in chosen_word:
-    if n == guess:
-        print("true")
-    else:
-        print("false")
+display = []
+
+for _ in chosen_word:
+    display += "_"
+    
+print(display)
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess the letter: ").lower()
+    
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+        
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win!")
