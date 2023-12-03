884195c8ef28fb3fa3a3013ea7c1f41d3d25ef8b
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2023-12-03 17:41:25 +0200

    Update prime_number.py

--- SRA/prime_number.py
+++ SRA/prime_number.py
@@ -1,17 +1,12 @@
 def prime_checker(number):
-    # If given number is greater than 1
-    if n > 1:
-	# Iterate from 2 to n / 2
-        for i in range(2, int(n/2)+1):
-		# If num is divisible by any number between
-		# 2 and n / 2, it is not prime
-            if (n % i) == 0:
-            	print(n, "It's not a prime number.")
-            	break
-        else:
-        	print(n, "It's a prime number.")
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print(n, "It's a prime number.")
     else:
         print(n, "It's not a prime number.")
 
-n = int(input("Give the number you want to test: ")) # Check this number
+n = int(input("Give the number you want to test: "))
 prime_checker(number=n)
