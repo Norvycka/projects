b0eeecd4a9fa2013b684f71329d1a1ee388aeff4
Vytenis Norkus <145903468+Norvycka@users.noreply.github.com>
2024-01-05 12:38:52 +0200

    learning to reverse a string without the easy way

---
+++ reversestring.py
@@ -0,0 +1,16 @@
+string = input("give me a word to reverse: ")
+
+cut = [i for i in string]
+
+length = len(cut)
+
+reversed_string = []
+
+for letter in range(length):
+    n=1
+    m = n + letter
+    jack = reversed_string.append(cut[length-m])
+print(reversed_string)
+
+jacker = string.reverse
+print(jacker)
